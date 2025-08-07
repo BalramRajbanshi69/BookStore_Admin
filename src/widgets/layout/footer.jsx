df32286a3fc8de007457f3ef052927e581ddcc83
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-08-08 01:28:51 +0545

    footer

--- src/widgets/layout/footer.jsx
+++ src/widgets/layout/footer.jsx
@@ -41,13 +41,13 @@ export function Footer({ brandName, brandLink, routes }) {
 }
 
 Footer.defaultProps = {
-  brandName: "Creative Tim",
-  brandLink: "https://www.creative-tim.com",
+  brandName: "Balram Rajbanshi",
+  brandLink: "balramworldprofile.vercel.app",
   routes: [
-    { name: "Creative Tim", path: "https://www.creative-tim.com" },
-    { name: "About Us", path: "https://www.creative-tim.com/presentation" },
-    { name: "Blog", path: "https://www.creative-tim.com/blog" },
-    { name: "License", path: "https://www.creative-tim.com/license" },
+    { name: "Balram Rajbanshi", path: "balramworldprofile.vercel.app" },
+    { name: "About Us", path: "balramworldprofile.vercel.app" },
+    { name: "Blog", path: "balramworldprofile.vercel.app" },
+    { name: "License", path: "balramworldprofile.vercel.app" },
   ],
 };
 
